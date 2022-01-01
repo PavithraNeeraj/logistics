ac860c98b50e20df1202d5a65e7c0547e7fab6d5
PavithraNeeraj <pavithraneeraj.golla@gmail.com>
2022-01-01 11:06:58 +0530

    PasswordFunctionality

--- src/org/soft/RockStar.java
+++ src/org/soft/RockStar.java
@@ -1,5 +1,11 @@
 package org.soft;
 
 public class RockStar {
+public static void password() {
+	System.out.println("Password : BeshuVedhik");
 
+}
+public static void main(String[] args) {
+	password();	
+}
 }
